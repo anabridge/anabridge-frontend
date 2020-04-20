f0f419c13ee4b4c877aab20d149f1fc6236ddc48
Martin <bbaalmart@gmail.com>
2020-04-20 12:09:49 +0300

    landing page and project set up

---
+++ bridge/src/views/components/LandingPage/LandingPage.js
@@ -0,0 +1,80 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer.js";
+import GridContainer from "../Grid/GridContainer.js";
+import GridItem from "../Grid/GridItem.js";
+import Button from "../CustomButtons/Button.js";
+import HeaderLinks from "../Header/HeaderLinks.js";
+import Parallax from "../Parallax/Parallax.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPage.js";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.js";
+import TeamSection from "./Sections/TeamSection.js";
+import WorkSection from "./Sections/WorkSection.js";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+export default function LandingPage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        color="transparent"
+        routes={dashboardRoutes}
+        brand="Bridge"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white",
+        }}
+        {...rest}
+      />
+      <Parallax filter image={require("assets/img/landing-bg.jpg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h4>
+                We utilize data to help organizations improve business processes
+                and make better investment decisions.
+              </h4>
+              <br />
+              <Button
+                color="danger"
+                size="lg"
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-play" />
+                Watch video
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <ProductSection />
+          <TeamSection />
+          <WorkSection />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
